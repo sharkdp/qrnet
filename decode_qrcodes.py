98fe179566a53c70d07faa5f6bfa3b0f4d817141
sharkdp <davidpeter@web.de>
2016-11-06 19:15:53 +0100

    Logging of both training and test accuracy.

--- decode_qrcodes.py
+++ decode_qrcodes.py
@@ -69,17 +69,18 @@ def importData(folder):
 train_images, train_labels = importData("train")
 test_images, test_labels = importData("test")
 
-x_image = tf.placeholder(tf.float32, shape=[None, IMAGE_SIZE, IMAGE_SIZE, 1], name="x_image")
-tf.image_summary('input_image', x_image, max_images=10)
-y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y_")
-
-# Model
+# Inputs
+with tf.name_scope("input"):
+    x_image = tf.placeholder(tf.float32, shape=[None, IMAGE_SIZE, IMAGE_SIZE, 1], name="x_image")
+    tf.image_summary('input_image', x_image, max_images=10)
+    y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y_")
 
 # First convolutional layer
 NUM_KERNELS_1 = 32
+KERNEL_SIZE_1 = 5
 
 with tf.name_scope("conv_1"):
-    W_conv1 = weight_variable([5, 5, 1, NUM_KERNELS_1])
+    W_conv1 = weight_variable([KERNEL_SIZE_1, KERNEL_SIZE_1, 1, NUM_KERNELS_1])
     b_conv1 = bias_variable([NUM_KERNELS_1])
 
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
@@ -87,9 +88,10 @@ with tf.name_scope("conv_1"):
 
 # Second convolutional layer
 NUM_KERNELS_2 = 64
+KERNEL_SIZE_2 = 5
 
 with tf.name_scope("conv_2"):
-    W_conv2 = weight_variable([5, 5, NUM_KERNELS_1, NUM_KERNELS_2])
+    W_conv2 = weight_variable([KERNEL_SIZE_2, KERNEL_SIZE_2, NUM_KERNELS_1, NUM_KERNELS_2])
     b_conv2 = bias_variable([NUM_KERNELS_2])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -99,10 +101,10 @@ with tf.name_scope("conv_2"):
 NUM_FULLY_CONNECTED_1 = 1024
 
 with tf.name_scope("fc_1"):
-    W_fc1 = weight_variable([5 * 5 * NUM_KERNELS_2, NUM_FULLY_CONNECTED_1])
+    W_fc1 = weight_variable([KERNEL_SIZE_2 * KERNEL_SIZE_2 * NUM_KERNELS_2, NUM_FULLY_CONNECTED_1])
     b_fc1 = bias_variable([NUM_FULLY_CONNECTED_1])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 5 * 5 * NUM_KERNELS_2])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, KERNEL_SIZE_2 * KERNEL_SIZE_2 * NUM_KERNELS_2])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # Dropout
@@ -111,7 +113,6 @@ with tf.name_scope("fc_1"):
 
 
 # Readout
-
 with tf.name_scope("readout"):
     W_fcR = weight_variable([NUM_FULLY_CONNECTED_1, 10])
     b_fcR = bias_variable([10])
@@ -127,66 +128,66 @@ with tf.name_scope("cross_entropy"):
 with tf.name_scope("train_step"):
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-with tf.name_scope("correct_prediction"):
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-
+# Evaluation
 with tf.name_scope("accuracy"):
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    tf.scalar_summary("accuracy", accuracy)
+    with tf.name_scope("correct_prediction"):
+        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    with tf.name_scope("accuracy"):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.scalar_summary("accuracy", accuracy)
 
 with tf.Session() as sess:
     # Summaries
     merged = tf.merge_all_summaries()
-    train_writer = tf.train.SummaryWriter('log', sess.graph)
 
-    sess.run(tf.initialize_all_variables())
+    train_writer = tf.train.SummaryWriter('log/train', sess.graph, flush_secs=5)
+    test_writer = tf.train.SummaryWriter('log/test', flush_secs=5)
+
+    tf.initialize_all_variables().run()
 
     saver = tf.train.Saver()
     # print("Loading model from checkpoint")
     # saver.restore(sess, "test.chk")
 
-    test_accuracy = accuracy.eval(feed_dict={
-                                  x_image: test_images,
-                                  y_: test_labels,
-                                  keep_prob: 1.0})
+    BATCH_SIZE = 20
+    MAX_STEPS = 40000
 
-    print("testing accuracy {:.03}".format(test_accuracy), flush=True)
-
-    batchSize = 20
-    for i in range(4000):
-        b = (batchSize * i) % 8000
-        batch = train_images[b:(b + batchSize)]
-        batch_labels = train_labels[b:(b + batchSize)]
+    for i in range(MAX_STEPS):
+        b = (BATCH_SIZE * i) % 8000
+        batch_images = train_images[b:(b + BATCH_SIZE)]
+        batch_labels = train_labels[b:(b + BATCH_SIZE)]
 
         if i % 100 == 0:
-            # train_accuracy = accuracy.eval(feed_dict={
-            #                                x_image: batch,
-            #                                y_: batch_labels,
-            #                                keep_prob: 1.0})
-            # print("step {:06}, training accuracy {:.03}".format(i * batchSize, train_accuracy))
-            # test_accuracy = accuracy.eval(feed_dict={
-            #                               x_image: test_images,
-            #                               y_: test_labels,
-            #                               keep_prob: 1.0})
-
+            # Record test set accuracy
             summary, acc = sess.run([merged, accuracy], feed_dict={
-                                    x_image: batch,
-                                    y_: batch_labels,
+                                    x_image: test_images,
+                                    y_: test_labels,
                                     keep_prob: 1.0})
-            print("{:06}\t{:.03}".format(i * batchSize, acc), flush=True)
-            train_writer.add_summary(summary, i)
+            print("Test accuracy at step {:06}: {:.03}".format(i, acc), flush=True)
+            test_writer.add_summary(summary, i)
+        else:
+            if i % 100 == 99:
+                # Record execution stats
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                summary, _ = sess.run([merged, train_step], feed_dict={
+                                      x_image: batch_images,
+                                      y_: batch_labels,
+                                      keep_prob: 0.5},
+                                      options=run_options,
+                                      run_metadata=run_metadata
+                                      )
+                train_writer.add_run_metadata(run_metadata, "step_{:06}".format(i))
+                train_writer.add_summary(summary, i)
+            else:
+                # Training
+                summary, _ = sess.run([merged, train_step], feed_dict={
+                                      x_image: batch_images,
+                                      y_: batch_labels,
+                                      keep_prob: 0.5})
+                train_writer.add_summary(summary, i)
 
-        summary, _ = sess.run([merged, train_step], feed_dict={
-                              x_image: batch,
-                              y_: batch_labels,
-                              keep_prob: 0.5})
-        train_writer.add_summary(summary, i)
+    train_writer.close()
+    test_writer.close()
 
-    saver.save(sess, "test.chk")
-
-    test_accuracy = accuracy.eval(feed_dict={
-                                  x_image: test_images,
-                                  y_: test_labels,
-                                  keep_prob: 1.0})
-
-    print("testing accuracy {:.03}".format(test_accuracy), flush=True)
+    saver.save(sess, "qrnet.chk")
